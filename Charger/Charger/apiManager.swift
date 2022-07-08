06d0119bcbd09fa5c441e19c6f61ca671307de7f
Nazlıcan Çay <nazlican1306@gmail.com>
2022-07-08 23:45:37 +0300

    chooseCitypage
    fix the chose city get request

--- Charger/Charger/apiManager.swift
+++ Charger/Charger/apiManager.swift
@@ -85,24 +85,30 @@ class ApiManager{
     func getCityNames(){
         print("----------------")
         print(UserInfo[2])
-       // let id : Int? = Int(UserInfo[2]))
-        let CityApi = URL(string: "ec2-18-197-100-203.eu-central-1.compute.amazonaws.com:8080/provinces?userID=" + "\(UserInfo[2])")
+        let id : Int? = Int(UserInfo[2])
+        let CityApi = URL(string: "ec2-18-197-100-203.eu-central-1.compute.amazonaws.com:8080/provinces?userID=" + "\(id)")
         if let unwrappedURL = CityApi {
             var request = URLRequest(url: unwrappedURL)
             request.addValue("\(UserInfo[1])", forHTTPHeaderField: "token")
-            let dataTask = URLSession.shared.dataTask(with: request) { (data, response, error) in
-                // you should put in error handling code, too
-                if let data = data {
-                    do {
-                        let json = try JSONDecoder().decode(CityResponse.self, from: data) as CityResponse
-                        // HERE'S WHERE YOUR DATA IS
-                        print(json.CityResponse.count)
-                    } catch {
-                        print(error.localizedDescription)
-                    }
-                }
-            }
-            dataTask.resume()
+            let defaultSession =  URLSession(configuration: .default)
+            let dataTask = defaultSession.dataTask(with: request){(data: Data? , responds :URLResponse? ,error :Error?) in
+                      
+                      if(error != nil){
+                          print(error)
+                          return
+                      }
+                      
+                      do{
+                          let json = try JSONDecoder().decode(CityResponse.self, from: data!)
+                          print(json.CityResponse.count)
+                      }
+                      catch {
+                          print(error)
+                          return
+                      }
+                          
+                  }
+                  dataTask.resume()
     }
     
    
